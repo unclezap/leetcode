df1377a669b7f20a4c9cd8fb7719f2c57f5529ea
unclezap <zeb.blackwell@gmail.com>
2020-07-17 20:07:17 -0700

    working with a hash that creates an array faster than working with hash alone

--- xKind.js
+++ xKind.js
@@ -14,6 +14,155 @@ let deck6 = [1,1]
 //true
 let deck7 = [0,0,0,0,0,1,1,1,1,1]
 //true
+let deck8 = [0,0,0,0,0,1,1,2,3,4]
+//false
+
+var mapHasGroupsSizeX2 = function(deck) {
+    if (deck.length === 1) {
+        return false
+    }
+
+    let map = new Map()
+
+    deck.forEach((number) => {
+        let times = map.get(number)
+        map.set(number, times ? times+1: 1) 
+    })
+
+    let max
+    let divides
+
+    if (Math.min(...map.values()) < 4) {
+        if (Math.min(...map.values()) === 1) {
+            return false
+        }
+        max = 3
+    } else {
+        max = Math.min(...map.values())/2
+    }
+
+    for (let i=2; i <= max; i++) {
+        divides = true
+        map.forEach((value) => {
+            if (value % i !== 0 ) {
+                divides = false
+            }
+        })
+        if (divides) {
+            return true
+        }
+    }
+
+    divides = true
+    map.forEach(value => {
+        if (value % Math.min(...map.values()) !== 0) {
+            divides = false
+        }
+    })
+
+    return divides
+}
+
+var hashHasGroupsSizeX2 = function(deck) {
+    if (deck.length === 1) {
+        return false
+    }
+
+    let hash = {}
+    deck.forEach((number) => {
+        if (hash[number]) {
+            hash[number]++
+        } else {
+            hash[number] = 1
+        }
+    })
+
+    let max
+    let divides
+
+    if (Math.min(...Object.values(hash)) < 4) {
+        if (Math.min(...Object.values(hash)) === 1) {
+            return false
+        }
+        max = 3
+    } else {
+        max = Math.min(...Object.values(hash))/2
+    }
+
+
+    for (let i=2; i <= max; i++) {
+        divides = true
+        for (let j=0; j < Object.values(hash).length; j++) {
+            if (Object.values(hash)[j] % i !== 0) {
+                divides = false
+                break
+            }
+        }
+        if (divides) {
+            return true
+        }
+    }
+
+    for (let j=0; j < Object.values(hash).length; j++) {
+        if (Object.values(hash)[j] % Math.min(...Object.values(hash)) !== 0) {
+            return false
+        }
+    }
+
+    return true
+}
+
+var hashHasGroupsSizeX2WithVariable = function(deck) {
+    if (deck.length === 1) {
+        return false
+    }
+
+    let hash = {}
+    deck.forEach((number) => {
+        if (hash[number]) {
+            hash[number]++
+        } else {
+            hash[number] = 1
+        }
+    })
+
+    let max
+    let divides
+    let values = Object.values(hash)
+
+    if (Math.min(...values) < 4) {
+        if (Math.min(...values) === 1) {
+            return false
+        }
+        max = 3
+    } else {
+        max = Math.min(...values)/2
+    }
+
+
+    for (let i=2; i <= max; i++) {
+        divides = true
+        for (let j=0; j < values.length; j++) {
+            if (values[j] % i !== 0) {
+                divides = false
+                break
+            }
+        }
+        if (divides) {
+            return true
+        }
+    }
+
+    for (let j=0; j < Object.values(hash).length; j++) {
+        if (values[j] % Math.min(...values) !== 0) {
+            return false
+        }
+    }
+
+    return true
+}
+
+
 
 var hasGroupsSizeX = function(deck) {
     if (deck.length === 1) {
@@ -34,7 +183,6 @@ var hasGroupsSizeX = function(deck) {
         }
     }
 
-    let nums = Object.keys(hash)
     let max
     let divides
 
@@ -62,8 +210,8 @@ var hasGroupsSizeX = function(deck) {
     }
 
     divides = true
-    for (let j=0; j < nums.length; j++) {
-        if (nums[j] % Math.min(...array) !== 0) {
+    for (let j=0; j < array.length; j++) {
+        if (array[j] % Math.min(...array) !== 0) {
             return false
         }
     }
@@ -71,10 +219,66 @@ var hasGroupsSizeX = function(deck) {
     return true
 };
 
-// console.log(hasGroupsSizeX(deck1))
-// console.log(hasGroupsSizeX(deck2))
-// console.log(hasGroupsSizeX(deck3))
-// console.log(hasGroupsSizeX(deck4))
-// console.log(hasGroupsSizeX(deck5))
-// console.log(hasGroupsSizeX(deck6))
-console.log(hasGroupsSizeX(deck7))
+console.log("=====")
+start = Date.now()
+for (u=0;u<100000;u++) {
+hasGroupsSizeX(deck1)
+hasGroupsSizeX(deck2)
+hasGroupsSizeX(deck3)
+hasGroupsSizeX(deck4)
+hasGroupsSizeX(deck5)
+hasGroupsSizeX(deck6)
+hasGroupsSizeX(deck7)
+hasGroupsSizeX(deck8)
+}
+end = Date.now()
+timeElapsed = end - start
+console.log(`first algo time: ${timeElapsed} ms`)
+
+console.log("=====")
+start = Date.now()
+for (u=0;u<100000;u++) {
+mapHasGroupsSizeX2(deck1)
+mapHasGroupsSizeX2(deck2)
+mapHasGroupsSizeX2(deck3)
+mapHasGroupsSizeX2(deck4)
+mapHasGroupsSizeX2(deck5)
+mapHasGroupsSizeX2(deck6)
+mapHasGroupsSizeX2(deck7)
+mapHasGroupsSizeX2(deck8)
+}
+end = Date.now()
+timeElapsed = end - start
+console.log(`hashmap time: ${timeElapsed} ms`)
+
+console.log("=====")
+start = Date.now()
+for (u=0;u<100000;u++) {
+hashHasGroupsSizeX2(deck1)
+hashHasGroupsSizeX2(deck2)
+hashHasGroupsSizeX2(deck3)
+hashHasGroupsSizeX2(deck4)
+hashHasGroupsSizeX2(deck5)
+hashHasGroupsSizeX2(deck6)
+hashHasGroupsSizeX2(deck7)
+hashHasGroupsSizeX2(deck8)
+}
+end = Date.now()
+timeElapsed = end - start
+console.log(`hash time: ${timeElapsed} ms`)
+
+console.log("=====")
+start = Date.now()
+for (u=0;u<100000;u++) {
+hashHasGroupsSizeX2WithVariable(deck1)
+hashHasGroupsSizeX2WithVariable(deck2)
+hashHasGroupsSizeX2WithVariable(deck3)
+hashHasGroupsSizeX2WithVariable(deck4)
+hashHasGroupsSizeX2WithVariable(deck5)
+hashHasGroupsSizeX2WithVariable(deck6)
+hashHasGroupsSizeX2WithVariable(deck7)
+hashHasGroupsSizeX2WithVariable(deck8)
+}
+end = Date.now()
+timeElapsed = end - start
+console.log(`hash with variable time: ${timeElapsed} ms`)
