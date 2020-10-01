3fb088e841c37a3bfecc682980e585b0055a97aa
unclezap <zeb.blackwell@gmail.com>
2020-09-30 17:06:28 -0700

    ip addresses exceptions fixed

--- ipAddresses.js
+++ ipAddresses.js
@@ -8,9 +8,13 @@ var validIPAddress = function(IP) {
             if (ipValues[i].match(/[0-9]/g) === null) {
                 return "Neither"
             }
-            if (ipValues[i].length === 0 || ipValues[i][0] === "0" || ipValues[i].split('').length !== ipValues[i].match(/[0-9]/g).length || ipValues[i] < 0 || ipValues[i] > 255) {
+            if (ipValues[i].length === 0 || ipValues[i].split('').length !== ipValues[i].match(/[0-9]/g).length || ipValues[i] < 0 || ipValues[i] > 255) {
                 return "Neither"
             }
+            if (ipValues[i].length > 1 && ipValues[i][0] === "0") {
+                return "Neither"
+            }
+            
         }
         return "IPv4"
     }
@@ -18,25 +22,31 @@ var validIPAddress = function(IP) {
     ipValues = IP.split(':')
     
     if (ipValues.length === 8) {
-        for (let i=0; i < 8; i++) {
             for (let i=0; i < 8; i++) {
-                console.log(ipValues[i])
+                if (ipValues[i].length > 4) {
+                    return "Neither"
+                }
                 if (ipValues[i].match(/[0-9]|[abcdef]/gi) === null) {
                     return "Neither"
                 }
                 if (ipValues[i].split('').length !== ipValues[i].match(/[0-9]|[abcdef]/gi).length) {
                     return "Neither"
                 }
-                // if (ipValues[i] === "85a3") {
-                //     console.log(ipValues[i].match(/0/g).length)
-                // }
-                if (ipValues[i].length > 1 && ipValues[i].split('').join('') === ipValues[i].match(/0/g).join('')) {
-                    return "Neither"
+                if (ipValues[i].length > 1 && ipValues[i][0] == 0) {
+                    let allZeros = true
+                    for (let j=1; j < ipValues[i].length; j++) {
+                        if (ipValues[i][j] !== '0') {
+                            allZeros = false
+                            break
+                        }
+                    }
+                    // if (allZeros) {
+                    //         return "Neither"
+                    // }
                 }
-                
             }
-        }
         return "IPv6"
     }
+    
     return "Neither"
 };
