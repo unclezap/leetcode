bac952ede839f8e957e410c170978ae5de79ffc5
unclezap <zeb.blackwell@gmail.com>
2020-10-25 23:31:59 -0700

    faster to count back than to use s.length

--- lengthLastWord.js
+++ lengthLastWord.js
@@ -4,9 +4,10 @@ var lengthOfLastWord = function(s) {
     let end = s.length - 1
     
     if (s[end] === " ") {
-        while (s[end] !== " ") {
+        while (s[end] === " ") {
             end--
         }
+        s = s.slice(0,end+1)
     }
     
     for (let i=end - 1; i >= 0; i--) {
@@ -15,5 +16,48 @@ var lengthOfLastWord = function(s) {
         }
     }
 
-    return end
+    return s.length
 };
+
+var lengthOfLastWordLeetcode100 = function(s) {
+    let input = s.trim();
+    let lastIndex = input.lastIndexOf(" ");
+    let result="";
+    if (input ==="") {return 0}
+    else if (lastIndex+1 >= s.length) {
+      result = input.substring(0, lastIndex)
+    } else {
+      result = input.substring(lastIndex+1,)
+    }
+    return result.length
+};
+
+console.log("=====")
+start = Date.now()
+for (u=0;u<10000000;u++) {
+    lengthOfLastWord("Hello World")
+    lengthOfLastWord("Hello World ")
+    lengthOfLastWord("World ")
+    lengthOfLastWord("World")
+    lengthOfLastWord("a")
+    lengthOfLastWord("a ")
+    lengthOfLastWord("")
+}
+end = Date.now()
+timeElapsed = end - start
+console.log(`third time: ${timeElapsed} ms`)
+
+console.log("=====")
+start = Date.now()
+for (u=0;u<10000000;u++) {
+    lengthOfLastWordLeetcode100("Hello World")
+    lengthOfLastWordLeetcode100("Hello World ")
+    lengthOfLastWordLeetcode100("World ")
+    lengthOfLastWordLeetcode100("World")
+    lengthOfLastWordLeetcode100("a")
+    lengthOfLastWordLeetcode100("a ")
+    lengthOfLastWordLeetcode100("")
+}
+end = Date.now()
+timeElapsed = end - start
+console.log(`third time: ${timeElapsed} ms`)
