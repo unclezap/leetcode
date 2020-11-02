82fc237c54d8d6fb71bde73445c5da5b622be978
unclezap <zeb.blackwell@gmail.com>
2020-11-02 13:00:11 -0800

    first java class and subclass

---
+++ Bicycle.java
@@ -0,0 +1,42 @@
+public class Bicycle {
+
+    public int cadence;
+    public int gear;
+    public int speed;
+
+    public Bicycle(int startCadence, int startSpeed, int startGear) {
+        gear = startGear;
+        cadence = startCadence;
+        speed = startSpeed;
+    }
+
+    public void setCadence(int newValue) {
+        cadence = newValue;
+    }
+
+    public void setGear(int newValue) {
+        gear = newValue
+    }
+
+    public void applyBrake(int decrement) {
+        speed -= decrement;
+    }
+
+    public void speedUp(int increment) {
+        speed += increment
+    }
+}
+
+public class MountainBike extends Bicycle {
+
+    public int seatHeight;
+
+    public MountainBike(int startHeight, int startCadence, int startSpeed, int startGear) {
+        super(startCadence, startSpeed, startGear);
+        seatHeight = startHeight;
+    }
+
+    public void setHeight(int newValue) {
+        seatHeight = newValue;
+    }
+}
