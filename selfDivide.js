2d4caceb92d102680fbc35890b818cbbfaff69e4
unclezap <zeb.blackwell@gmail.com>
2020-11-15 23:56:53 -0800

    modulo arithmetic is the easy way to strip off digits

--- selfDivide.js
+++ selfDivide.js
@@ -1,23 +1,26 @@
 // https://leetcode.com/problems/self-dividing-numbers/
 
+
 var selfDividingNumbers = function(left, right) {
     let returnArray = []
     let number
+    let digit
     let tensPower = 0
     
     for (let i=left; i <= right; i++) {
         number = i
         let works = true
-        while (10**tensPower <= number) {
-            if (number % 10**(tensPower + 1) === 0 || i % (number % 10**(tensPower + 1)) !== 0) {
+        while (number >= 1) {
+            digit = number % 10
+            if (digit === 0 || i % digit !== 0) {
                 works = false
                 break
             }
-            number -= number % 10**(tensPower + 1)
-            tensPower++
+            number -= digit
+            number /= 10
         }
         if (works) {
-            returnArray.push(number)
+            returnArray.push(i)
         }
     }
     
