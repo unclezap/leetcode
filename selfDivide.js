61483410d7bfaecdb2c487095a259a7f3ba0799f
unclezap <zeb.blackwell@gmail.com>
2020-11-15 23:52:55 -0800

    dad

--- selfDivide.js
+++ selfDivide.js
@@ -1,10 +1,25 @@
 // https://leetcode.com/problems/self-dividing-numbers/
 
 var selfDividingNumbers = function(left, right) {
-    let returnArray
-    let digit
+    let returnArray = []
+    let number
+    let tensPower = 0
     
     for (let i=left; i <= right; i++) {
-        if  
+        number = i
+        let works = true
+        while (10**tensPower <= number) {
+            if (number % 10**(tensPower + 1) === 0 || i % (number % 10**(tensPower + 1)) !== 0) {
+                works = false
+                break
+            }
+            number -= number % 10**(tensPower + 1)
+            tensPower++
+        }
+        if (works) {
+            returnArray.push(number)
+        }
     }
+    
+    return returnArray
 };
