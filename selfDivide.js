78f9c64e47e5784bd0000dc2de91d5a94128126b
unclezap <zeb.blackwell@gmail.com>
2020-11-15 23:46:20 -0800

    attempting

---
+++ selfDivide.js
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/self-dividing-numbers/
+
+var selfDividingNumbers = function(left, right) {
+    let returnArray
+    let digit
+    
+    for (let i=left; i <= right; i++) {
+        if  
+    }
+};
