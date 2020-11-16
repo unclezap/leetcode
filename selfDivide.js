d5a6a0935d2ab273dede89e0b3cfb86c88a08a59
unclezap <zeb.blackwell@gmail.com>
2020-11-16 00:05:11 -0800

    leetcode for some reason processes strings way faster than they have any right to

--- selfDivide.js
+++ selfDivide.js
@@ -5,7 +5,6 @@ var selfDividingNumbers = function(left, right) {
     let returnArray = []
     let number
     let digit
-    let tensPower = 0
     
     for (let i=left; i <= right; i++) {
         number = i
@@ -26,3 +25,77 @@ var selfDividingNumbers = function(left, right) {
     
     return returnArray
 };
+
+//this seems like it'd be really slow actually
+const selfDividingNumbersLeetcode100 = (left, right) => {
+    let store = [];
+    let digit;
+    let count;
+    for (let i = left; i <= right; i++) {
+        digit = i.toString().split('');
+        count = 0;
+        for (const d of digit) if (i % d === 0) count++;
+        if (count === digit.length) store.push(parseInt(i));
+    }
+    return store;
+}
+
+var selfDividingNumbersLeetcodeSecond = function(left, right) {
+    const res = [];
+    for (let i = left; i <= right; i++) {
+        if (i < 10) {
+            res.push(i);
+            continue;
+        }
+        if (i.toString().includes('0')) {
+            continue;
+        }
+        let n = i;
+        let isDividable = true;
+        while (n) {
+            let digit = n % 10;
+            if (i % digit) {
+                isDividable = false;
+                break;
+            }
+            n = Math.floor(n / 10);
+        }
+        if (isDividable) {
+            res.push(i);
+        }
+    }
+}
+    
+
+console.log("=====")
+start = Date.now()
+for (u=0;u<10000;u++) {
+    selfDividingNumbers(1,22)
+    selfDividingNumbers(1,3)
+    selfDividingNumbers(105,220)
+}
+end = Date.now()
+timeElapsed = end - start
+console.log(`min/max time: ${timeElapsed} ms`)
+
+console.log("=====")
+start = Date.now()
+for (u=0;u<10000;u++) {
+    selfDividingNumbersLeetcode100(1,22)
+    selfDividingNumbersLeetcode100(1,3)
+    selfDividingNumbersLeetcode100(105,220)
+}
+end = Date.now()
+timeElapsed = end - start
+console.log(`min/max time: ${timeElapsed} ms`)
+
+console.log("=====")
+start = Date.now()
+for (u=0;u<10000;u++) {
+    selfDividingNumbersLeetcode100(1,22)
+    selfDividingNumbersLeetcode100(1,3)
+    selfDividingNumbersLeetcode100(105,220)
+}
+end = Date.now()
+timeElapsed = end - start
+console.log(`min/max time: ${timeElapsed} ms`)
