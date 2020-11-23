601c8644ca46369847dfe604e8795ff2696d715c
unclezap <zeb.blackwell@gmail.com>
2020-11-22 23:49:57 -0800

    merge

--- sorting.js
+++ sorting.js
@@ -19,5 +19,37 @@ function bubbleSort (list) {
     if (repeat) bubbleSort(list)
 }
 
-bubbleSort(list)
+// bubbleSort(list)
+// console.log(list)
+
+function mergeSort () {
+    if (list.length <= 1) {
+        return list
+    }
+
+    let middle = list.length/2
+    let left = list.slice(0, middle)
+    let right = list.slice(middle, list.length)
+
+    return merge(mergeSort(left), mergeSort(right))
+
+    function merge (left, right) {
+        let result = []
+
+        while (left.length || right.length) {
+            if (left.length && right.length) {
+                if (left[0] < right[0]) {
+                    result.push(right.shift())
+                }
+            } else if (left.length) {
+                result.push(left.shift())
+            } else {
+                result.push(right.shift())
+            }
+        }
+        return result
+    }
+}
+
+mergeSort(list)
 console.log(list)
