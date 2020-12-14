c02695a1298a12ff327db8ae9f026c4c0e205a98
unclezap <zeb.blackwell@gmail.com>
2020-12-13 23:11:09 -0800

    completed

--- factory.cs
+++ factory.cs
@@ -6,9 +6,20 @@ public class Traveler
     public string Name { get; set;}
     public string FavoriteColor { get; set;}
     public string AverageAirSpeedOfUnladenedSwallow {get; set;}
+    public List<Order> Orders {get; set;}
 
     public static Traveler From Defaults ()
     {
         var traveler = new Customer { Status = "unapproved", Name = "Travler"}
+        traveler.Orders = new List<Order> ();
+        return traveler;
+    }
+
+    public static Traveler FromExisting (IDictionary values)
+    {
+
+        var traveler = new Traveler ();
+
+        return traveler
     }
 }
