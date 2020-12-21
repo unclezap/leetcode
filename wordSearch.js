a6fc0866b2cc0aa8057b9ffe181e0aa40b0c9934
unclezap <zeb.blackwell@gmail.com>
2020-12-20 23:53:27 -0800

    bones

--- wordSearch.js
+++ wordSearch.js
@@ -1,1 +1,22 @@
 // https://leetcode.com/problems/word-search/
+
+var exist = function(board, word) {
+    let wordPos = 0
+    
+    for (let i=0;i<board.length;i++) {
+        for (let j=0;j<board[i].length;j++) {
+            if (board[i][j] === word[wordPos]) {
+                if (snake(i,j,board,word,wordPos)) {
+                    return true
+                }
+            }
+        }
+    }
+    
+    return false
+};
+
+var snake = function(yPosition, xPosition, board, word, wordPos) {
+    
+    
+}
