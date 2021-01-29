03e1065c1d21128cbe00acbd85d2e526a256d47c
unclezap <zeb.blackwell@gmail.com>
2021-01-28 17:13:31 -0800

    improvements:

--- buddyStrings.js
+++ buddyStrings.js
@@ -28,7 +28,49 @@ var buddyStrings = function(A, B) {
          if (A.length === 2 && A[0] === A[1]) {
              return true
          }
-         let doubles = false
+         let hash = {}
+         for (let i=0;i<A.length;i++) {
+             if (!hash[A[i]]) {
+                hash[A[i]] = 1
+             } else {
+                return true
+             }
+         }
+     }
+     
+     return false
+ };
+
+ var buddyStrings2 = function(A, B) {
+    if (A.length !== B.length || A.length < 2) {
+        return false
+    }
+ 
+     let indices = []
+     
+     for (let i=0;i<A.length;i++) {
+         if (A[i] !== B[i]) {
+            if (indices.length === 1) {
+                if (A[indices[0]] !== B[i] || A[i] !== B[indices[0]]) {
+                    return false
+                }
+            }
+            if (indices.length === 2) {
+                return false
+            }
+            indices.push(i)
+
+         }
+     }
+ 
+     if (indices.length === 2) {
+             return true
+     }
+ 
+     if (indices.length === 0) {
+         if (A.length === 2 && A[0] === A[1]) {
+             return true
+         }
          let hash = {}
          for (let i=0;i<A.length;i++) {
              if (!hash[A[i]]) {
@@ -89,7 +131,7 @@ var buddyStrings = function(A, B) {
 
 console.log("=====")
 start = Date.now()
-for (u=0;u<1000000;u++) {
+for (u=0;u<20000000;u++) {
     buddyStrings("ab","ba")
     buddyStrings("ab","ab")
     buddyStrings("aa","aa")
@@ -101,10 +143,24 @@ end = Date.now()
 timeElapsed = end - start
 console.log(`min/max time: ${timeElapsed} ms`)
 
+console.log("=====")
+start = Date.now()
+for (u=0;u<20000000;u++) {
+    buddyStrings2("ab","ba")
+    buddyStrings2("ab","ab")
+    buddyStrings2("aa","aa")
+    buddyStrings2("aaaaabc","aaaaacb")
+    buddyStrings2("","ba")
+    buddyStrings2("aab","aab")
+}
+end = Date.now()
+timeElapsed = end - start
+console.log(`min/max time: ${timeElapsed} ms`)
+
 
 console.log("=====")
 start = Date.now()
-for (u=0;u<1000000;u++) {
+for (u=0;u<20000000;u++) {
     buddyStringsLeetcode100("ab","ba")
     buddyStringsLeetcode100("ab","ab")
     buddyStringsLeetcode100("aa","aa")
